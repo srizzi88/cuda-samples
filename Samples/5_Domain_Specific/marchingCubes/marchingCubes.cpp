5f19fc35359a7af05ccfb8e776529af3828c40d7
srizzi <srizzi@anl.gov>
2024-06-10 15:47:12 -0500

    more code cleaning to remove unused parts

--- Samples/5_Domain_Specific/marchingCubes/marchingCubes.cpp
+++ Samples/5_Domain_Specific/marchingCubes/marchingCubes.cpp
@@ -80,21 +80,13 @@
   Using number of vertices from readback.
 */
 
-//#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
-//#define WINDOWS_LEAN_AND_MEAN
-//#define NOMINMAX
-//#include <windows.h>
-//#endif
-
 // includes
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-//#include <helper_gl.h>
 
 #include <cuda_runtime.h>
-//#include <cuda_gl_interop.h>
 #include <vector_types.h>
 #include <vector_functions.h>
 
@@ -102,13 +94,6 @@
 #include <helper_functions.h>
 
 #include "defines.h"
-
-//#if defined(__APPLE__) || defined(MACOSX)
-//#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-//#include <GLUT/glut.h>
-//#else
-//#include <GL/freeglut.h>
-//#endif
 
 extern "C" void launch_classifyVoxel(dim3 grid, dim3 threads, uint *voxelVerts,
                                      uint *voxelOccupied, uchar *volume,
@@ -140,10 +125,6 @@ extern "C" void destroyAllTextureObjects();
 extern "C" void ThrustScanWrapper(unsigned int *output, unsigned int *input,
                                   unsigned int numElements);
 
-// constants
-//const unsigned int window_width = 512;
-//const unsigned int window_height = 512;
-
 const char *volumeFilename = "Bucky.raw";
 
 uint3 gridSizeLog2 = make_uint3(5, 5, 5);
@@ -160,9 +141,6 @@ uint totalVerts = 0;
 float isoValue = 0.2f;
 float dIsoValue = 0.005f;
 
-// device data
-//GLuint posVbo, normalVbo;
-//GLint gl_Shader;
 struct cudaGraphicsResource *cuda_posvbo_resource,
     *cuda_normalvbo_resource;  // handles OpenGL-CUDA exchange
 
@@ -180,42 +158,17 @@ uint *d_numVertsTable = 0;
 uint *d_edgeTable = 0;
 uint *d_triTable = 0;
 
-// mouse controls
-//int mouse_old_x, mouse_old_y;
-//int mouse_buttons = 0;
-//float3 rotate = make_float3(0.0, 0.0, 0.0);
-//float3 translate = make_float3(0.0, 0.0, -3.0);
-
-// toggles
-//bool wireframe = false;
-//bool animate = true;
-//bool lighting = true;
-//bool render = true;
 //bool compute = true;
 
 #define MAX_EPSILON_ERROR 5.0f
 #define REFRESH_DELAY 10  // ms
 
-// Define the files that are to be save and the reference images for validation
-//const char *sOriginal[] = {"march_cubes.ppm", NULL};
-
-//const char *sReference[] = {"ref_march_cubes.ppm", NULL};
-
-//StopWatchInterface *timer = 0;
-
-// Auto-Verification Code
-//const int frameCheckNumber = 4;
-//int fpsCount = 0;  // FPS count for averaging
-//int fpsLimit = 1;  // FPS limit for sampling
-//int g_Index = 0;
-//unsigned int frameCount = 0;
 bool g_bValidate = false;
 
 int *pArgc = NULL;
 char **pArgv = NULL;
 
 // forward declarations
-//void runGraphicsTest(int argc, char **argv);
 void runAutoTest(int argc, char **argv);
 void initMC(int argc, char **argv);
 void computeIsosurface();
@@ -223,61 +176,12 @@ void dumpFile(void *dData, int data_bytes, const char *file_name);
 
 template <class T>
 void dumpBuffer(T *d_buffer, int nelements, int size_element);
-
 void cleanup();
 
-//bool initGL(int *argc, char **argv);
-//void createVBO(GLuint *vbo, unsigned int size);
-//void deleteVBO(GLuint *vbo, struct cudaGraphicsResource **cuda_resource);
-
-//void display();
-//void keyboard(unsigned char key, int x, int y);
-//void mouse(int button, int state, int x, int y);
-//void motion(int x, int y);
-//void idle();
-//void reshape(int w, int h);
-
-void mainMenu(int i);
+//void mainMenu(int i);
 
 #define EPSILON 5.0f
 #define THRESHOLD 0.30f
-
-//void animation() {
-//  if (animate) {
-//    isoValue += dIsoValue;
-
-//    if (isoValue < 0.1f) {
-//      isoValue = 0.1f;
-//      dIsoValue *= -1.0f;
-///   } else if (isoValue > 0.9f) {
-//      isoValue = 0.9f;
-//      dIsoValue *= -1.0f;
-//    }
-//  }
-//}
-
-//void timerEvent(int value) {
-//  animation();
-//  glutPostRedisplay();
-//  glutTimerFunc(REFRESH_DELAY, timerEvent, 0);
-//}
-
-//void computeFPS() {
-//  frameCount++;
-//  fpsCount++;
-
-//  if (fpsCount == fpsLimit) {
-//    char fps[256];
-//    float ifps = 1.f / (sdkGetAverageTimerValue(&timer) / 1000.f);
-//    sprintf(fps, "CUDA Marching Cubes: %3.1f fps", ifps);
-
-//    glutSetWindowTitle(fps);
-//    fpsCount = 0;
-
-//    fpsLimit = ftoi(MAX(1.f, ifps));
-//    sdkResetTimer(&timer);
-//  }
-//}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Load raw data from disk
@@ -380,16 +284,10 @@ int main(int argc, char **argv) {
   pArgc = &argc;
   pArgv = argv;
 
-//#if defined(__linux__)
-//  setenv("DISPLAY", ":0", 0);
-//#endif
-
   printf("[%s] - Starting...\n", argv[0]);
 
   if (checkCmdLineFlag(argc, (const char **)argv, "file") &&
       checkCmdLineFlag(argc, (const char **)argv, "dump")) {
-//    animate = false;
-//    fpsLimit = frameCheckNumber;
     g_bValidate = true;
     runAutoTest(argc, argv);
   } else {
@@ -520,56 +418,6 @@ void cleanup() {
   }
 }
 
-//void initMenus() {
-//  glutCreateMenu(mainMenu);
-//  glutAddMenuEntry("Toggle animation [ ]", ' ');
-//  glutAddMenuEntry("Increment isovalue [+]", '+');
-//  glutAddMenuEntry("Decrement isovalue [-]", '-');
-//  glutAddMenuEntry("Toggle computation [c]", 'c');
-//  glutAddMenuEntry("Toggle rendering [r]", 'r');
-//  glutAddMenuEntry("Toggle lighting [l]", 'l');
-//  glutAddMenuEntry("Toggle wireframe [w]", 'w');
-//  glutAddMenuEntry("Quit (esc)", '\033');
-//  glutAttachMenu(GLUT_RIGHT_BUTTON);
-//}
-
-//void runGraphicsTest(int argc, char **argv) {
-//  printf("MarchingCubes\n");
-
-//  if (checkCmdLineFlag(argc, (const char **)argv, "device")) {
-//    printf("[%s]\n", argv[0]);
-//    printf("   Does not explicitly support -device=n in OpenGL mode\n");
-//    printf("   To use -device=n, the sample must be running w/o OpenGL\n\n");
-//    printf(" > %s -device=n -file=<reference> -dump=<0/1/2>\n", argv[0]);
-//    exit(EXIT_SUCCESS);
-//  }
-
-  // First initialize OpenGL context, so we can properly set the GL for CUDA.
-  // This is necessary in order to achieve optimal performance with OpenGL/CUDA
-  // interop.
-//  if (false == initGL(&argc, argv)) {
-//    return;
-//  }
-
-//  findCudaDevice(argc, (const char **)argv);
-
-  // register callbacks
-//  glutDisplayFunc(display);
-//  glutKeyboardFunc(keyboard);
-//  glutMouseFunc(mouse);
-//  glutMotionFunc(motion);
-//  glutReshapeFunc(reshape);
-//  glutTimerFunc(REFRESH_DELAY, timerEvent, 0);
-//  initMenus();
-
-  // Initialize CUDA buffers for Marching Cubes
-//  initMC(argc, argv);
-
-//  sdkCreateTimer(&timer);
-
-  // start rendering mainloop
-//  glutMainLoop();
-//}
 
 #define DEBUG_BUFFERS 0
 
@@ -653,18 +501,18 @@ void computeIsosurface() {
 
   // generate triangles, writing to vertex buffers
   if (!g_bValidate) {
-    size_t num_bytes;
+    //size_t num_bytes;
     // DEPRECATED: checkCudaErrors(cudaGLMapBufferObject((void**)&d_pos,
     // posVbo));
-    checkCudaErrors(cudaGraphicsMapResources(1, &cuda_posvbo_resource, 0));
-    checkCudaErrors(cudaGraphicsResourceGetMappedPointer(
-        (void **)&d_pos, &num_bytes, cuda_posvbo_resource));
+    //checkCudaErrors(cudaGraphicsMapResources(1, &cuda_posvbo_resource, 0));
+    //checkCudaErrors(cudaGraphicsResourceGetMappedPointer(
+    //    (void **)&d_pos, &num_bytes, cuda_posvbo_resource));
 
     // DEPRECATED: checkCudaErrors(cudaGLMapBufferObject((void**)&d_normal,
     // normalVbo));
-    checkCudaErrors(cudaGraphicsMapResources(1, &cuda_normalvbo_resource, 0));
-    checkCudaErrors(cudaGraphicsResourceGetMappedPointer(
-        (void **)&d_normal, &num_bytes, cuda_normalvbo_resource));
+    //checkCudaErrors(cudaGraphicsMapResources(1, &cuda_normalvbo_resource, 0));
+    //checkCudaErrors(cudaGraphicsResourceGetMappedPointer(
+    //    (void **)&d_normal, &num_bytes, cuda_normalvbo_resource));
   }
 
 #if SKIP_EMPTY_VOXELS
@@ -692,285 +540,9 @@ void computeIsosurface() {
 
   if (!g_bValidate) {
     // DEPRECATED:      checkCudaErrors(cudaGLUnmapBufferObject(normalVbo));
-    checkCudaErrors(cudaGraphicsUnmapResources(1, &cuda_normalvbo_resource, 0));
+    //checkCudaErrors(cudaGraphicsUnmapResources(1, &cuda_normalvbo_resource, 0));
     // DEPRECATED:      checkCudaErrors(cudaGLUnmapBufferObject(posVbo));
-    checkCudaErrors(cudaGraphicsUnmapResources(1, &cuda_posvbo_resource, 0));
+    //checkCudaErrors(cudaGraphicsUnmapResources(1, &cuda_posvbo_resource, 0));
   }
 }
 
-// shader for displaying floating-point texture
-//static const char *shader_code =
-//    "!!ARBfp1.0\n"
-//    "TEX result.color, fragment.texcoord, texture[0], 2D; \n"
-//    "END";
-
-//GLuint compileASMShader(GLenum program_type, const char *code) {
-//  GLuint program_id;
-//  glGenProgramsARB(1, &program_id);
-//  glBindProgramARB(program_type, program_id);
-//  glProgramStringARB(program_type, GL_PROGRAM_FORMAT_ASCII_ARB,
-//                     (GLsizei)strlen(code), (GLubyte *)code);
-
-//  GLint error_pos;
-//  glGetIntegerv(GL_PROGRAM_ERROR_POSITION_ARB, &error_pos);
-
-//  if (error_pos != -1) {
-//    const GLubyte *error_string;
-//    error_string = glGetString(GL_PROGRAM_ERROR_STRING_ARB);
-//    fprintf(stderr, "Program error at position: %d\n%s\n", (int)error_pos,
-//            error_string);
-//    return 0;
-//  }
-
-//  return program_id;
-//}
-
-////////////////////////////////////////////////////////////////////////////////
-//! Initialize OpenGL
-////////////////////////////////////////////////////////////////////////////////
-//bool initGL(int *argc, char **argv) {
-  // Create GL context
-//  glutInit(argc, argv);
-//  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-//  glutInitWindowSize(window_width, window_height);
-//  glutCreateWindow("CUDA Marching Cubes");
-
-//  if (!isGLVersionSupported(2, 0)) {
-//    fprintf(stderr, "ERROR: Support for necessary OpenGL extensions missing.");
-//    fflush(stderr);
-//    return false;
-//  }
-
-  // default initialization
-//  glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
-//  glEnable(GL_DEPTH_TEST);
-
-  // good old-fashioned fixed function lighting
-//  float black[] = {0.0f, 0.0f, 0.0f, 1.0f};
-//  float white[] = {1.0f, 1.0f, 1.0f, 1.0f};
-//  float ambient[] = {0.1f, 0.1f, 0.1f, 1.0f};
-//  float diffuse[] = {0.9f, 0.9f, 0.9f, 1.0f};
-//  float lightPos[] = {0.0f, 0.0f, 1.0f, 0.0f};
-
-//  glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
-//  glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
-//  glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, black);
-
-//  glLightfv(GL_LIGHT0, GL_AMBIENT, white);
-//  glLightfv(GL_LIGHT0, GL_DIFFUSE, white);
-//  glLightfv(GL_LIGHT0, GL_SPECULAR, white);
-//  glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
-
-//  glLightModelfv(GL_LIGHT_MODEL_AMBIENT, black);
-
-//  glEnable(GL_LIGHT0);
-//  glEnable(GL_NORMALIZE);
-
-  // load shader program
-//  gl_Shader = compileASMShader(GL_FRAGMENT_PROGRAM_ARB, shader_code);
-
-//  glutReportErrors();
-
-//  return true;
-//}
-
-////////////////////////////////////////////////////////////////////////////////
-//! Create VBO
-////////////////////////////////////////////////////////////////////////////////
-//void createVBO(GLuint *vbo, unsigned int size) {
-//  // create buffer object
-//  glGenBuffers(1, vbo);
-//  glBindBuffer(GL_ARRAY_BUFFER, *vbo);
-
-  // initialize buffer object
-//  glBufferData(GL_ARRAY_BUFFER, size, 0, GL_DYNAMIC_DRAW);
-//  glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-//  glutReportErrors();
-//}
-
-////////////////////////////////////////////////////////////////////////////////
-//! Delete VBO
-////////////////////////////////////////////////////////////////////////////////
-//void deleteVBO(GLuint *vbo, struct cudaGraphicsResource **cuda_resource) {
-//  glBindBuffer(1, *vbo);
-//  glDeleteBuffers(1, vbo);
-  // DEPRECATED: checkCudaErrors(cudaGLUnregisterBufferObject(*vbo));
-//  cudaGraphicsUnregisterResource(*cuda_resource);
-
-//  *vbo = 0;
-//}
-
-////////////////////////////////////////////////////////////////////////////////
-// Render isosurface geometry from the vertex buffers
-////////////////////////////////////////////////////////////////////////////////
-//void renderIsosurface() {
-//  glBindBuffer(GL_ARRAY_BUFFER, posVbo);
-//  glVertexPointer(4, GL_FLOAT, 0, 0);
-//  glEnableClientState(GL_VERTEX_ARRAY);
-
-//  glBindBuffer(GL_ARRAY_BUFFER, normalVbo);
-//  glNormalPointer(GL_FLOAT, sizeof(float) * 4, 0);
-//  glEnableClientState(GL_NORMAL_ARRAY);
-
-//  glColor3f(1.0, 0.0, 0.0);
-//  glDrawArrays(GL_TRIANGLES, 0, totalVerts);
-//  glDisableClientState(GL_VERTEX_ARRAY);
-//  glDisableClientState(GL_NORMAL_ARRAY);
-
-//  glBindBuffer(GL_ARRAY_BUFFER, 0);
-//}
-
-////////////////////////////////////////////////////////////////////////////////
-//! Display callback
-////////////////////////////////////////////////////////////////////////////////
-//void display() {
-//  sdkStartTimer(&timer);
-
-  // run CUDA kernel to generate geometry
-//  if (compute) {
-//    computeIsosurface();
-//  }
-
-  // Common display code path
-//  {
-//    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // set view matrix
-//    glMatrixMode(GL_MODELVIEW);
-//    glLoadIdentity();
-//    glTranslatef(translate.x, translate.y, translate.z);
-//    glRotatef(rotate.x, 1.0, 0.0, 0.0);
-//    glRotatef(rotate.y, 0.0, 1.0, 0.0);
-
-//    glPolygonMode(GL_FRONT_AND_BACK, wireframe ? GL_LINE : GL_FILL);
-
-//    if (lighting) {
-//      glEnable(GL_LIGHTING);
-//    }
-
-    // render
-//    if (render) {
-//      glPushMatrix();
-//      glRotatef(180.0, 0.0, 1.0, 0.0);
-//      glRotatef(90.0, 1.0, 0.0, 0.0);
-//      renderIsosurface();
-//      glPopMatrix();
-//    }
-
-//    glDisable(GL_LIGHTING);
-//  }
-
-//  glutSwapBuffers();
-//  glutReportErrors();
-
-//  sdkStopTimer(&timer);
-
-//  computeFPS();
-//}
-
-////////////////////////////////////////////////////////////////////////////////
-//! Keyboard events handler
-////////////////////////////////////////////////////////////////////////////////
-//void keyboard(unsigned char key, int /*x*/, int /*y*/) {
-//  switch (key) {
-//    case (27):
-//      cleanup();
-//      exit(EXIT_SUCCESS);
-
-//    case '=':
-//      isoValue += 0.01f;
-//      break;
-
-//    case '-':
-//      isoValue -= 0.01f;
-//      break;
-
-//    case '+':
-//      isoValue += 0.1f;
-//      break;
-
-//    case '_':
-//      isoValue -= 0.1f;
-//      break;
-
-//    case 'w':
-//      wireframe = !wireframe;
-//      break;
-
-//    case ' ':
-//      animate = !animate;
-//      break;
-
-//    case 'l':
-//      lighting = !lighting;
-//      break;
-
-//    case 'r':
-//      render = !render;
-//      break;
-
-//    case 'c':
-//      compute = !compute;
-//      break;
-//  }
-
-//  printf("isoValue = %f\n", isoValue);
-//  printf("voxels = %d\n", activeVoxels);
-//  printf("verts = %d\n", totalVerts);
-//  printf("occupancy: %d / %d = %.2f%%\n", activeVoxels, numVoxels,
-//         activeVoxels * 100.0f / (float)numVoxels);
-
-//  if (!compute) {
-//    computeIsosurface();
-//  }
-//}
-
-////////////////////////////////////////////////////////////////////////////////
-//! Mouse event handlers
-////////////////////////////////////////////////////////////////////////////////
-//void mouse(int button, int state, int x, int y) {
-//  if (state == GLUT_DOWN) {
-//    mouse_buttons |= 1 << button;
-//  } else if (state == GLUT_UP) {
-//    mouse_buttons = 0;
-//  }
-
-//  mouse_old_x = x;
-//  mouse_old_y = y;
-//}
-
-//void motion(int x, int y) {
-//  float dx = (float)(x - mouse_old_x);
-//  float dy = (float)(y - mouse_old_y);
-
-//  if (mouse_buttons == 1) {
-//    rotate.x += dy * 0.2f;
-//    rotate.y += dx * 0.2f;
-//  } else if (mouse_buttons == 2) {
-//    translate.x += dx * 0.01f;
-//    translate.y -= dy * 0.01f;
-//  } else if (mouse_buttons == 3) {
-//    translate.z += dy * 0.01f;
-//  }
-
-//  mouse_old_x = x;
-//  mouse_old_y = y;
-//  glutPostRedisplay();
-//}
-
-//void idle() {
-//  animation();
-//  glutPostRedisplay();
-//}
-
-//void reshape(int w, int h) {
-//  glMatrixMode(GL_PROJECTION);
-//  glLoadIdentity();
-//  gluPerspective(60.0, (float)w / (float)h, 0.1, 10.0);
-
-//  glMatrixMode(GL_MODELVIEW);
-//  glViewport(0, 0, w, h);
-//}
-
-//void mainMenu(int i) { keyboard((unsigned char)i, 0, 0); }
